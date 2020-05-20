482c791116cabebb6a8ca8d21910bf07b1c71e9b
Fabian <fabianserna2013@gmail.com>
2020-05-19 19:08:31 -0500

    Updates

---
+++ precache-manifest.1b5a60a754705cc5f438cafa0e155cab.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c156be4c82b5ec62d2a7b5490f3d048",
+    "url": "/cursostoc/index.html"
+  },
+  {
+    "revision": "d6cf24f2b9b1bc2c1248",
+    "url": "/cursostoc/static/css/2.59405167.chunk.css"
+  },
+  {
+    "revision": "e424dce4ccc3b2a1c4ea",
+    "url": "/cursostoc/static/css/main.d8a0e08f.chunk.css"
+  },
+  {
+    "revision": "d6cf24f2b9b1bc2c1248",
+    "url": "/cursostoc/static/js/2.369bdc6d.chunk.js"
+  },
+  {
+    "revision": "ece9bd75801d2a43ab7b0478f8f1bf03",
+    "url": "/cursostoc/static/js/2.369bdc6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e424dce4ccc3b2a1c4ea",
+    "url": "/cursostoc/static/js/main.3da8d477.chunk.js"
+  },
+  {
+    "revision": "1cd995aff499be694639",
+    "url": "/cursostoc/static/js/runtime-main.2538466f.js"
+  },
+  {
+    "revision": "578ccad8b7b4001282f272ebd418c18b",
+    "url": "/cursostoc/static/media/click.578ccad8.png"
+  },
+  {
+    "revision": "9d892e7388bc1146a70810e7c89d1697",
+    "url": "/cursostoc/static/media/comment.9d892e73.png"
+  },
+  {
+    "revision": "780803f5c7f19070fb08a6e34775701d",
+    "url": "/cursostoc/static/media/documents.780803f5.png"
+  },
+  {
+    "revision": "321d4f8ecf339db355ab28d38bb14de3",
+    "url": "/cursostoc/static/media/lens.321d4f8e.png"
+  },
+  {
+    "revision": "6027d4f16e8ef63f79f23c14db91ebba",
+    "url": "/cursostoc/static/media/logo.6027d4f1.png"
+  },
+  {
+    "revision": "b286b60dc49bc489c96c11154afafb1b",
+    "url": "/cursostoc/static/media/phone.b286b60d.png"
+  },
+  {
+    "revision": "7b2cede09f9776088c40bc0ba7cf03bd",
+    "url": "/cursostoc/static/media/points.7b2cede0.png"
+  },
+  {
+    "revision": "cf6bbe7f533bbd2135c3fa1ad091befd",
+    "url": "/cursostoc/static/media/puzzle.cf6bbe7f.png"
+  },
+  {
+    "revision": "65128d90fcc4781307966899c1f327c6",
+    "url": "/cursostoc/static/media/statistics.65128d90.png"
+  },
+  {
+    "revision": "78e8389154939d11dd3d37a519f069c1",
+    "url": "/cursostoc/static/media/statistics2.78e83891.png"
+  },
+  {
+    "revision": "290a2465ece6c9564a21f838bfe56fa5",
+    "url": "/cursostoc/static/media/touch.290a2465.png"
+  },
+  {
+    "revision": "0ecfbad61b7f78040eabb35f033ad8e6",
+    "url": "/cursostoc/static/media/touch2.0ecfbad6.png"
+  },
+  {
+    "revision": "2fe16f0f603db1c51484b4f831ebff21",
+    "url": "/cursostoc/static/media/upload.2fe16f0f.svg"
+  },
+  {
+    "revision": "fd184c5c2a6a586b061acee338727791",
+    "url": "/cursostoc/static/media/whatsapp.fd184c5c.jpg"
+  }
+]);
